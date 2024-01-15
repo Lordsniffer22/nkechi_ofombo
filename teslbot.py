04bad7f080139cd2b25e21b8e4c6da76f3762af7
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-01-15 04:54:30 +0300

    Update teslbot.py

--- teslbot.py
+++ teslbot.py
@@ -73,10 +73,10 @@ def list_users(chat_id):
 
             # Exclude users with expiry set to "never"
             if remaining_days.lower() != 'never':
-                users_days_remaining.append(f"│ {user}   ⇿ {remaining_days}")
+                users_days_remaining.append(f"│ {user}   ⇿  {remaining_days}")
 
         users_message = "\n".join(users_days_remaining)
-        return f"╭─👩🏻‍🦰USERS──────EXPIRY DATES─╮\n{users_message} \n╰───────────────────────╯"
+        return f"╭─👩🏻‍🦰USERS─────🕗EXPIRY DATES─╮\n{users_message} \n╰───────────────────────╯"
     except subprocess.CalledProcessError as e:
         return f"Failed to list users. Error: {e}"
 
