ec0aa29246713322c4390e383658fbf6e071bfaa
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-01-20 17:39:11 +0300

    Update botx.py

--- botx.py
+++ botx.py
@@ -1,73 +1,66 @@
 import telepot
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from youtube_dl import YoutubeDL
+from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent
+from youtube_search import YoutubeSearch  # You can install it with: pip install youtube-search
+import youtube_dl
 
-TOKEN = '6724007051:AAG_ZXO7N__TwMQlVFvJuuJmJ1ViBIRWchY'
+# Telegram bot API token
+TELEGRAM_API_TOKEN = '6724007051:AAG_ZXO7N__TwMQlVFvJuuJmJ1ViBIRWchY'
 
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
+# Youtube DL options for downloading in mp3 format
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'extractaudio': True,
+    'audioformat': 'mp3',
+    'outtmpl': '%(title)s.%(ext)s',
+    'noplaylist': True,
+}
 
-    if content_type == 'text':
-        search_query = msg['text']
+# Function to handle inline queries
+def on_inline_query(msg):
+    query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
 
-        # Perform YouTube search
-        videos = search_youtube(search_query)
+    # Search YouTube videos based on the user's query
+    videos = YoutubeSearch(query_string, max_results=5).to_dict()
 
-        if videos:
-            # Create inline keyboard with video options
-            keyboard = create_inline_keyboard(videos)
-
-            # Send message with inline keyboard
-            bot.sendMessage(chat_id, 'Select a video:', reply_markup=keyboard)
-        else:
-            bot.sendMessage(chat_id, 'No videos found for the given query.')
-
-def search_youtube(query):
-    # Use youtube_dl to search for videos
-    ydl_opts = {'format': 'best', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}]}
-    with YoutubeDL(ydl_opts) as ydl:
-        try:
-            result = ydl.extract_info(f'ytsearch:{query}', download=False)
-            if 'entries' in result:
-                return result['entries']
-        except Exception as e:
-            print(f'Error searching YouTube: {e}')
-    return None
-
-def create_inline_keyboard(videos):
-    # Create inline keyboard with video options
-    keyboard = []
+    results = []
     for video in videos:
-        title = video.get('title', 'Unknown Title')
-        video_id = video.get('id', '')
-        keyboard.append([InlineKeyboardButton(text=title, callback_data=f'download_{video_id}')])
+        title = video['title']
+        link = f"https://www.youtube.com/watch?v={video['id']}"
+        thumb_url = video['thumbnails'][0]
 
-    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+        # Create an InlineQueryResultArticle with a button to download in mp3
+        results.append(InlineQueryResultArticle(
+            id=video['id'],
+            title=title,
+            input_message_content=InputTextMessageContent(
+                message_text=f"Click the button to download [**{title}**]({link}) in mp3 format."),
+            reply_markup={'inline_keyboard': [[{'text': 'Download Mp3', 'callback_data': video['id']}]]},
+            thumb_url=thumb_url
+        ))
 
+    # Answer the inline query with the search results
+    bot.answerInlineQuery(query_id, results)
+
+
+# Function to handle callback queries (i.e., when the user clicks on the download button)
 def on_callback_query(msg):
-    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    query_id, from_id, data = telepot.glance(msg, flavor='callback_query')
 
-    if query_data.startswith('download_'):
-        video_id = query_data.split('_')[1]
-        download_and_send_audio(from_id, video_id)
+    # Download the selected video in mp3 format
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        video_info = ydl.extract_info(f'https://www.youtube.com/watch?v={data}', download=True)
+        audio_url = video_info['formats'][0]['url']
 
-def download_and_send_audio(chat_id, video_id):
-    # Download and send the audio file to the user
-    ydl_opts = {'format': 'best', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}]}
-    with YoutubeDL(ydl_opts) as ydl:
-        try:
-            result = ydl.extract_info(video_id, download=True)
-            if 'entries' in result:
-                audio_file = result['entries'][0]['title'] + '.mp3'
-                bot.sendAudio(chat_id, open(audio_file, 'rb'))
-        except Exception as e:
-            print(f'Error downloading audio: {e}')
+        # Send the mp3 file to the user
+        bot.sendAudio(from_id, audio_url, title=video_info['title'])
 
-# Initialize the bot and set callback function
-bot = telepot.Bot(TOKEN)
-bot.message_loop({'chat': handle, 'callback_query': on_callback_query})
 
-print('Listening for messages...')
-import time
-while 1:
-    time.sleep(10)
+# Create the Telegram bot
+bot = telepot.Bot(TELEGRAM_API_TOKEN)
+
+# Set the callback functions
+bot.message_loop({'inline_query': on_inline_query, 'callback_query': on_callback_query})
+
+print('Bot is listening...')
+while True:
+    pass
