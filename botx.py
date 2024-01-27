c5e88e152d1fcc0d9e59767a00a647668c62f709
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-01-27 15:06:35 +0300

    Update botx.py

--- botx.py
+++ botx.py
@@ -9,24 +9,22 @@ TELEGRAM_BOT_TOKEN = '6643175652:AAH6haOsyYIUmw6ql8U_5-Bmdocguwzwolc'
 # Replace 'YOUR_YOUTUBE_API_KEY' with your YouTube API key
 YOUTUBE_API_KEY = 'AIzaSyAa-43K1ZF6xYlbPW7A7ufHTpgzkUwsGas'
 
-def on_inline_query(msg):
-    query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
+def handle_messages(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
 
-    results = search_youtube(query_string)
+    if content_type == 'text':
+        process_text_message(chat_id, msg['text'])
 
-    articles = [
-        InlineQueryResultArticle(
-            id=str(idx),
-            title=result['title'],
-            input_message_content=InputTextMessageContent(
-                message_text=f"Title: {result['title']}\n"
-                             f"Video ID: {result['video_id']}\n"
-                             f"Link: https://www.youtube.com/watch?v={result['video_id']}"
-            )
-        ) for idx, result in enumerate(results)
-    ]
-
-    bot.answerInlineQuery(query_id, articles)
+def process_text_message(chat_id, text):
+    if text.startswith('@'):
+        # Autocomplete command and start listening for YouTube search queries
+        bot.sendMessage(chat_id, f"Youtube search {text}")
+    else:
+        username = bot.getMe()['username']
+        if f'@{username}' in text:
+            search_query = text.replace(f'@{username}', '').strip()
+            results = search_youtube(search_query)
+            send_youtube_results(chat_id, results)
 
 def search_youtube(query):
     youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
@@ -40,8 +38,30 @@ def search_youtube(query):
 
     return results
 
+def send_youtube_results(chat_id, results):
+    for result in results:
+        message = f"Title: {result['title']}\n"
+        message += f"Video ID: {result['video_id']}\n"
+        message += f"Link: https://www.youtube.com/watch?v={result['video_id']}"
+        bot.sendMessage(chat_id, message)
+
+def on_inline_query(msg):
+    query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
+    
+    # Generate a single article with the autocomplete suggestion
+    articles = [InlineQueryResultArticle(
+        id='1',
+        title='YouTube search',
+        input_message_content=InputTextMessageContent(
+            message_text=f'Youtube search @{query_string}',
+            parse_mode='Markdown'
+        )
+    )]
+
+    bot.answerInlineQuery(query_id, articles)
+
 bot = telepot.Bot(TELEGRAM_BOT_TOKEN)
-MessageLoop(bot, {'inline_query': on_inline_query}).run_as_thread()
+MessageLoop(bot, {'chat': handle_messages, 'inline_query': on_inline_query}).run_as_thread()
 
 print('Bot is listening...')
 
