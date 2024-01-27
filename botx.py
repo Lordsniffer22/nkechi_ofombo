399155849f99d55140f530803eecaa6d088bf3be
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-01-27 14:24:42 +0300

    Update botx.py

--- botx.py
+++ botx.py
@@ -1,20 +1,43 @@
 import telepot
-from telepot.loop import MessageLoop
+from telepot.loop import MessageLoop, InlineUserHandler
 from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent
 from googleapiclient.discovery import build
-# Replace 'YOUR_BOT_TOKEN' with the token obtained from BotFather
-TELEGRAM_BOT_TOKEN = '6643175652:AAH6haOsyYIUmw6ql8U_5-Bmdocguwzwolc'
 
-# Replace 'YOUR_YOUTUBE_API_KEY' with your YouTube API key
+TELEGRAM_BOT_TOKEN = '6643175652:AAH6haOsyYIUmw6ql8U_5-Bmdocguwzwolc'
 YOUTUBE_API_KEY = 'AIzaSyAa-43K1ZF6xYlbPW7A7ufHTpgzkUwsGas'
 
 def handle_messages(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
 
-    if content_type == 'inline_query':
-        query_id, query_text, _ = telepot.glance(msg, flavor='inline_query')
-        results = search_youtube(query_text)
-        answer_inline_query(query_id, results)
+    if content_type == 'text':
+        username = bot.getMe()['username']
+        if f'@{username}' in msg['text']:
+            search_query = msg['text'].replace(f'@{username}', '').strip()
+            results = search_youtube(search_query)
+            send_youtube_results(chat_id, results)
+
+def on_inline_query(msg):
+    query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
+    results = search_youtube(query_string)
+    articles = []
+
+    for idx, result in enumerate(results):
+        title = result['title']
+        video_id = result['video_id']
+        link = f"https://www.youtube.com/watch?v={video_id}"
+        content = f"{title}\n{link}"
+
+        articles.append(
+            InlineQueryResultArticle(
+                id=str(idx),
+                title=title,
+                input_message_content=InputTextMessageContent(
+                    message_text=content
+                )
+            )
+        )
+
+    bot.answerInlineQuery(query_id, articles)
 
 def search_youtube(query):
     youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
@@ -24,28 +47,21 @@ def search_youtube(query):
     for item in search_response['items']:
         title = item['snippet']['title']
         video_id = item['id']['videoId']
-        results.append(
-            InlineQueryResultArticle(
-                id=video_id,
-                title=title,
-                input_message_content=InputTextMessageContent(
-                    message_text=f"Title: {title}\n"
-                                 f"Video ID: {video_id}\n"
-                                 f"Link: https://www.youtube.com/watch?v={video_id}"
-                )
-            )
-        )
+        results.append({'title': title, 'video_id': video_id})
 
     return results
 
-def answer_inline_query(query_id, results):
-    bot.answerInlineQuery(query_id, results, cache_time=0)
+def send_youtube_results(chat_id, results):
+    for result in results:
+        message = f"Title: {result['title']}\n"
+        message += f"Video ID: {result['video_id']}\n"
+        message += f"Link: https://www.youtube.com/watch?v={result['video_id']}"
+        bot.sendMessage(chat_id, message)
 
 bot = telepot.Bot(TELEGRAM_BOT_TOKEN)
-MessageLoop(bot, handle_messages).run_as_thread()
+MessageLoop(bot, {'chat': handle_messages, 'inline_query': on_inline_query}).run_as_thread()
 
 print('Bot is listening...')
 
 while True:
     pass
-
