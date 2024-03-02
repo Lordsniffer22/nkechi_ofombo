4a7c19ced56539726e1a686107dc8e500dcc78c6
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-03 02:31:51 +0300

    Update bot_runner.sh

--- bot_runner.sh
+++ bot_runner.sh
@@ -53,12 +53,8 @@ restart_bot() {
     ban_me
     print_center -ama "RESTARTING THE BOT".....
     sleep 2
-    if screen -ls | grep -q "Tesla"; then
-      screen -ls | grep Tesla | cut -d. -f1 | awk '{print $1}' | xargs kill
-    else
-      echo ""
-    fi
-    run_bot
+    systemctl daemon-reload &>/dev/null
+    systemctl restart sshbt &>/dev/null
     echo ""
     sleep 2
     bot_menu
@@ -69,14 +65,11 @@ run_bot() {
     ban_me
     print_center -ama "BOT INITIALISING....."
     sleep 3
-    if screen -ls | grep -q "Tesla"; then
-      screen -ls | grep Tesla | cut -d. -f1 | awk '{print $1}' | xargs kill
-    else
-      echo ""
-    fi
-    cd /etc/hsm/toxic/
-    screen -dmS Tesla_SSH_BOT /usr/bin/python3 olwa.py
-    print_pink "Cheers! Your bot is now running."
+    chmod 640 /etc/systemd/system/sshbt.service
+    systemctl daemon-reload &>/dev/null
+    systemctl enable sshbt &>/dev/null
+    systemctl start sshbt &>/dev/null
+    pri0nt_pink "Cheers! Your bot is now running."
     echo ""
     sleep 2
     sudo bot
@@ -87,12 +80,8 @@ stop_bot() {
     #Run the bot
     ban_me
     print_center -ama "STOPPING THE BOT".....
+    systemctl stop sshbt
     sleep 2
-    if screen -ls | grep -q "Tesla"; then
-      screen -ls | grep Tesla | cut -d. -f1 | awk '{print $1}' | xargs kill
-    else
-      echo ""
-    fi
     print_pink "Your Bot has been stopped"
     sleep 2
     bot_menu
@@ -122,24 +111,22 @@ ch_token() {
     bot_menu
 }
 
-bot_update() {
+bot_remove() {
   ban_me
-  print_center -ama "UPDATING THE BOT".....
+  print_center -ama "Removing. Please wait...."
+  sleep 3
   sudo rm -f /etc/hsm/toxic/olwa.py
-  wget -O olwa.py https://raw.githubusercontent.com/Lordsniffer22/nkechi_ofombo/main/teslbot.py &>/dev/null
   sudo rm -f /usr/bin/bot
-  wget -O /usr/bin/bot 'https://raw.githubusercontent.com/Lordsniffer22/nkechi_ofombo/main/bot_runner.sh' &>/dev/null
-  chmod +x /usr/bin/bot
-  sleep 4
-  print_pink "Your bot has been Updated successfully"
-  restart_bot
+  print_pink "Your bot has been Uninstalled Successfully"
+  sleep 3
+  clear
+  sudo udp
 }
 bot_install() {
     cd
     clear
-
     #sudo apt update && apt upgrade -y
-    sudo apt-get install screen
+   # sudo apt-get install screen
     sudo apt install python3-pip
     sudo pip install telepot &>/dev/null
     sudo pip install telepot --upgrade &>/dev/null
@@ -159,10 +146,7 @@ bot_install() {
     sudo rm -f /usr/bin/bot
     wget -O /usr/bin/bot 'https://raw.githubusercontent.com/Lordsniffer22/nkechi_ofombo/main/bot_runner.sh' &>/dev/null
     chmod +x /usr/bin/bot
-    
-    #get teslbot service from git
-    #wget -O teslbot.service https://raw.githubusercontent.com/TeslaSSH/Redq/main/config/teslbot.service &>/dev/null
-    #sudo mv teslbot.service /etc/systemd/system/
+
     clear
     print_center -ama "BOT TOKEN REQUIRED"
     sleep 2
@@ -170,7 +154,7 @@ bot_install() {
     echo ""
     read -p "Enter Token: " btoken
     sleep 2
-    print_center -ama "Thanks for entering your Telegram Bot Token"
+    print_center -ama "You entered: \e[1;95m$btoken\e[0m"
     sleep 2
     clear
 
@@ -214,6 +198,23 @@ bot_install() {
       do
         rm -f "$file"
       done
+    
+    # [make service file]
+    echo '[Unit]
+    Description=Made by Teslassh (( ZERO ONE LLC ))
+    After=network.target
+
+    [Service]
+    User=root
+    Type=simple
+    ExecStart=/usr/bin/python3 /etc/hsm/toxic/olwa.py 
+    WorkingDirectory=/etc/hsm/toxic/
+    Restart=always
+
+    [Install]
+    WantedBy=multi-user.target' > /etc/systemd/system/sshbt.service
+
+    # Activate the service
     run_bot
 }
 #bot_installer() {
@@ -240,52 +241,96 @@ ban_me() {
   print_pink "  |_| |_____|____/|_____/_/   \_\ |____/____/|_| |_|" 
   echo ""
 }
+#check system
+os_check() {
+  if [ -f /etc/os-release ]; then
+      . /etc/os-release
+      if [[ "$NAME" = "Ubuntu" && "$VERSION_ID" = "22.04" ]]; then
+          bot_install
+      elif [[ "$NAME" = "Ubuntu" && "$VERSION_ID" = "23.10" ]]; then
+          bot_install
+      else
+          print_pink "THE BOT IS MEANT TO RUN ON UBUNTU 22.04 AND 23.10"
+          exit 1
+      fi
+  fi 
+}
+install_udp_first() {
+    print_center 'After the server reboots, Login amd type "bot"'
+    sleep 4
+    rm -f install.sh
+    wget --no-cache  "https://raw.githubusercontent.com/TeslaSSH/Tesla_UDP_custom-/main/install.sh" -O install.sh
+    chmod +x install.sh 
+    ./install.sh
+}
+
+menu_real() {
+    ban_me
+    msg -bar
+    print_center -ama "BOT MANAGER By TeslaSSH"
+    msg -bar0
+    echo ""
+    # print options menu
+    print_center -ama "${a12:-CHOOSE AN OPTION}"
+    msg -bar3
+    echo " $(msg -verd "[1]") $(msg -verm2 '>') $(msg -ama "${a6:-RESTART BOT ☢️}")"
+    echo " $(msg -verd "[2]") $(msg -verm2 '>') $(msg -ama "${a8:-Install UDP BOT }")"
+    echo " $(msg -verd "[3]") $(msg -verm2 '>') $(msg -teal "${a11:-SECRET KEY 🔑}")"
+    echo " $(msg -verd "[4]") $(msg -verm2 '>') $(msg -ama "${a6:-STOP BOT ⛔}")"
+    echo " $(msg -verd "[5]") $(msg -verm2 '>') $(msg -ama "${a6:-CHANGE BOT TOKEN 🔁}")"
+    echo " $(msg -verd "[6]") $(msg -verm2 '>') $(msg -ama "${a6:-Uninstall Bot}")"
+    exit2home
+
+    # prompt user for option selection
+    read -p " ⇢  Enter your selection: " option
+
+    # handle option selection
+    case $option in
+    1)
+      restart_bot
+      ;;
+    2)
+      os_check #checks os and installs bot
+      ;;
+    3)
+      see_key
+      ;;
+    4)
+      stop_bot
+      ;;
+    5)
+      ch_token
+      ;;
+    6)
+      bot_remove
+      ;;
+    0)
+      exit
+      ;;
+    esac
+}
 
 bot_menu() {
   source <(curl -sSL 'https://raw.githubusercontent.com/TeslaSSH/Tesla_UDP_custom-/main/module/module')
-  ban_me
-  msg -bar
-  print_center -ama "BOT MANAGER By TeslaSSH"
-  msg -bar0
-  echo ""
-  # print options menu
-  print_center -ama "${a12:-CHOOSE AN OPTION}"
-  msg -bar3
-  echo " $(msg -verd "[1]") $(msg -verm2 '>') $(msg -ama "${a6:-RESTART BOT ☢️}")"
-  echo " $(msg -verd "[2]") $(msg -verm2 '>') $(msg -ama "${a8:-INSTALL BOT ✳️}")"
-  echo " $(msg -verd "[3]") $(msg -verm2 '>') $(msg -teal "${a11:-SECRET KEY 🔑}")"
-  echo " $(msg -verd "[4]") $(msg -verm2 '>') $(msg -ama "${a6:-STOP BOT ⛔}")"
-  echo " $(msg -verd "[5]") $(msg -verm2 '>') $(msg -ama "${a6:-CHANGE BOT TOKEN 🔁}")"
-  echo " $(msg -verd "[6]") $(msg -verm2 '>') $(msg -ama "${a6:-UPDATE BOT ♻️}")"
-  exit2home
-
-  # prompt user for option selection
-  read -p " ⇢  Enter your selection: " option
-
-  # handle option selection
-  case $option in
-  1)
-    restart_bot
-    ;;
-  2)
-    bot_install
-    ;;
-  3)
-    see_key
-    ;;
-  4)
-    stop_bot
-    ;;
-  5)
-    ch_token
-    ;;
-  6)
-    bot_update
-    ;;
-  0)
-    exit
-    ;;
-  esac
+  report=$(systemctl is-active udp-custom)
+  if [ "$report" == "active" ] || [ -f /etc/hsm/toxic/olwa.py ]; then
+      menu_real
+  else
+      print_pink "IT APPEARS THAT UDP CUSTOM IS NOT INSTALLED YET ON THIS VPS"
+      echo ""
+      echo " $(msg -verd "[1]") $(msg -verm2 '>') $(msg -ama "${a8:-Install UDP CUSTOM First✳️}")"
+      echo " $(msg -verd "[0]") $(msg -verm2 '>') $(msg -ama "${a8:-Exit installer}")"
+      echo ""
+      read -p " ⇢  Enter your selection: " ansa
+      case $ansa in
+      1)
+        install_udp_first
+        ;;
+      0)
+        exit
+        ;;
+      esac
+   fi   
 }
 
 bot_menu
