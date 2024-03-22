a2ab342d499add9e70b410df8d17a5310969e0d7
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-22 14:20:50 +0300

    Update botx.py

--- botx.py
+++ botx.py
@@ -9,33 +9,33 @@ def handle_message(msg):
     if content_type == 'text':
         query = msg['text']
         if is_youtube_link(query):
-            # If the message is a YouTube link, download and send the video in 360p MP4 format
-            send_video_file(chat_id, query)
+            # If the message is a YouTube link, download and send the MP4 video file
+            send_mp4_file(chat_id, query)
 
 # Function to check if a message is a YouTube link
 def is_youtube_link(text):
     return text.startswith('https://www.youtube.com/')
 
-# Function to download a YouTube video in 360p MP4 format
+# Function to download a YouTube video in MP4 format
 def download_video(video_url):
     yt = YouTube(video_url)
     video_title = yt.title
-    stream = yt.streams.filter(res="360p", file_extension="mp4").first()
+    stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
     if stream:
         file_path = stream.download()
-        video_file = f"{video_title}.mp4"
-        os.rename(file_path, video_file)
-        return video_file
+        mp4_file = f"{video_title}.mp4"
+        os.rename(file_path, mp4_file)
+        return mp4_file
     else:
         return None
 
-# Function to send a video file to the user
-def send_video_file(chat_id, video_url):
-    video_file = download_video(video_url)
-    if video_file:
-        with open(video_file, 'rb') as f:
+# Function to send an MP4 video file to the user
+def send_mp4_file(chat_id, video_url):
+    mp4_file = download_video(video_url)
+    if mp4_file:
+        with open(mp4_file, 'rb') as f:
             bot.sendVideo(chat_id, f)
-        os.remove(video_file)  # Remove the video file after sending
+        os.remove(mp4_file)  # Remove the MP4 file after sending
 
 # Set up the bot
 TOKEN = '7021922965:AAHIt6RrH6Tw4mVHh_QLCe-OpakH03igMvk'  # Replace with your actual bot token
