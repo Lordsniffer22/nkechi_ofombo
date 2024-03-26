507f8e5c1ec36aa52f4966a57eebbe92f25c6d42
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-26 17:55:41 +0300

    Update botx.py

--- botx.py
+++ botx.py
@@ -9,7 +9,7 @@ def handle_message(msg):
     if content_type == 'text':
         query = msg['text']
         if is_youtube_link(query):
-            processing = "Processing...\nHang on tight🤙"
+            processing = "Processing... \n Hang on tight🤙"
             processing_message = bot.sendMessage(chat_id, processing)
             if query.startswith('/v'):
                 send_video(chat_id, query)
@@ -26,21 +26,21 @@ def is_youtube_link(text):
 def download_video(video_url):
     yt = YouTube(video_url)
     video_title = yt.title
-    stream = yt.streams.first()
-    if stream:
-        file_path = stream.download()
-        video_file = f"{video_title}.{stream.subtype}"
-        os.rename(file_path, video_file)
-        return video_file
+    streams = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if streams:
+        file_path = streams.download()
+        return file_path, video_title
     else:
-        return None
+        return None, None
 
-# Function to send a video to the user
+# Function to send a video file to the user with a caption
 def send_video(chat_id, video_url):
-    video_file = download_video(video_url)
+    video_file, video_title = download_video(video_url)
     if video_file:
+        # Add a caption to the video file
+        caption = "Hey, your video is here.\n\n➤Bot: @tubyDoo_Bot \n│\n╰┈➤Join @udpcustom"
         with open(video_file, 'rb') as f:
-            bot.sendVideo(chat_id, f)
+            bot.sendVideo(chat_id, f, caption=caption)
         os.remove(video_file)  # Remove the video file after sending
 
 # Function to download a YouTube video and convert it to MP3
@@ -52,16 +52,16 @@ def download_and_convert_to_mp3(video_url):
         file_path = stream.download()
         mp3_file = f"{video_title}.mp3"
         os.rename(file_path, mp3_file)
-        return mp3_file
+        return mp3_file, video_title
     else:
-        return None
+        return None, None
 
-# Function to send an MP3 audio to the user with a caption
+# Function to send an MP3 file to the user with a caption
 def send_audio(chat_id, video_url):
-    mp3_file = download_and_convert_to_mp3(video_url)
+    mp3_file, video_title = download_and_convert_to_mp3(video_url)
     if mp3_file:
         # Add a caption to the audio file
-        caption = "Hey your music is here.\n\n➤Bot: @tubyDoo_Bot \n│\n╰┈➤Join @udpcustom"
+        caption = "Hey, your music is here.\n\n➤Bot: @tubyDoo_Bot \n│\n╰┈➤Join @udpcustom"
         with open(mp3_file, 'rb') as f:
             bot.sendAudio(chat_id, f, caption=caption)
         os.remove(mp3_file)  # Remove the MP3 file after sending
