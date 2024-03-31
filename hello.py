5f2224956a95aefd9f6c11a5f74a735968cff7fd
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-31 12:05:04 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("BOT_TOKEN")
+TOKEN = getenv("6533833584:AAHPalg1HywEshspXgeGAYOjWRG95jx8X4Q")
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
