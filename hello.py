930b3371586d785af3e7adcf8d91fc4ad0aaf08c
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-31 16:38:49 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,16 +4,13 @@ import sys
 from os import getenv
 from pytube import YouTube
 from aiogram import Bot, Dispatcher, types
-
-from aiogram.utils.markdown import hbold
 from aiogram.types import InputFile
-
+from aiogram.utils.markdown import hbold
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = '7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o'
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
-
 
 async def download_and_convert_to_mp3(video_url: str) -> str:
     """
@@ -34,7 +31,6 @@ async def download_and_convert_to_mp3(video_url: str) -> str:
         logging.error(f"Error downloading and converting to MP3: {e}")
         return None
 
-
 async def send_mp3_file(chat_id: int, video_url: str) -> None:
     """
     Send an MP3 file to the user with a caption
@@ -43,10 +39,13 @@ async def send_mp3_file(chat_id: int, video_url: str) -> None:
     if mp3_file:
         # Add a caption to the audio file
         caption = "Hey your music is here.\n\n➤Bot: @tubyDoo_Bot \n│\n╰┈➤Join @udpcustom"
-        with open(mp3_file, 'rb') as f:
-            audio = InputFile(f)
-            await bot.send_audio(chat_id, audio, caption=caption)
-        os.remove(mp3_file)  # Remove the MP3 file after sending
+        # Create an instance of InputFile using the mp3_file path
+        input_file = InputFile(mp3_file)
+        # Send the audio with the caption
+        await bot.send_audio(chat_id, input_file, caption=caption)
+        # Remove the MP3 file after sending
+        os.remove(mp3_file)
+
 
 
 @dp.message()
@@ -68,7 +67,7 @@ async def message_handler(message: types.Message) -> None:
 
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(TOKEN, parse_mode=types.ParseMode.HTML)
     # And the run events dispatching
     await dp.start_polling(bot)
 
