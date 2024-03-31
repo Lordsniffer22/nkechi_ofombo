3a01a7bd3d922a82377ff8df9216ca5a5fa6db6d
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-31 14:35:56 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,50 +2,56 @@ import aiogram
 import asyncio
 import logging
 import sys
+from os import getenv
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
-from aiogram.dispatcher.filters import CommandStart
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = '7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o'
 
-# Initialize Bot instance
-bot = Bot(token=TOKEN)
-# Initialize Dispatcher instance
-dp = Dispatcher(bot)
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
 
 
-@dp.message_handler(CommandStart())
+@dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    # Send a welcome message using message.answer method
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
 
-@dp.message_handler()
+@dp.message()
 async def echo_handler(message: types.Message) -> None:
     """
-    Handler will forward received message back to the sender
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
     try:
         # Send a copy of the received message
-        await message.copy_to(chat_id=message.chat.id)
+        await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        # Not all types are supported to be copied, handle it
+        # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
 
 
 async def main() -> None:
-    # Start polling for updates
-    await dp.start_polling()
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    # Configure logging
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    # Run main function asynchronously
     asyncio.run(main())
