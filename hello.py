6b59a92beb06bbd24b9e0f2d158c2a3f0974fc4c
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-31 15:46:40 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,75 +3,82 @@ import asyncio
 import logging
 import sys
 from os import getenv
+from pytube import YouTube
 
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
-from aiogram.utils.markdown import hbold
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = '7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o'
-
-# All handlers should be attached to the Router (or Dispatcher)
+# All handlers should be attached to the Dispatcher
 dp = Dispatcher()
 
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
+@dp.message_handler(content_types=['text'])
+async def handle_message(message: Message) -> None:
     """
-    This handler receives messages with `/start` command
+    Function to handle incoming messages
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    query = message.text
+    if is_youtube_link(query):
+        processing = "Processing...\nHang on tight🤙"
+        processing_message = await message.reply(processing)
+        await send_mp3_file(message.chat.id, query)
+        await message.delete()
+        await processing_message.delete()
 
 
-@dp.message(commands=['add_user'])
-async def add_user_handler(message: types.Message) -> None:
+def is_youtube_link(text):
     """
-    Handler for adding a user
+    Function to check if a message is a YouTube link
     """
-    # Extract user information from the command
-    try:
-        command, username, password, days = message.text.split()
-        days = int(days)
-    except ValueError:
-        await message.answer("Invalid command format. Please use: /add_user [username] [password] [days]")
-        return
-
-    # Add user using the provided logic
-    result_message = add_user(username, password, days, user_info=f"{username}'s account", chat_id=message.chat.id)
-
-    # Send a response to the user
-    await message.answer(result_message)
+    return text.startswith('https://www.youtube.com/') or text.startswith('https://youtu.be/')
 
 
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
+async def download_and_convert_to_mp3(video_url):
     """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    Function to download a YouTube video and convert it to MP3
     """
     try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+        yt = YouTube(video_url)
+        video_title = yt.title
+        stream = yt.streams.filter(only_audio=True).first()
+        if stream:
+            file_path = stream.download()
+            mp3_file = f"{video_title}.mp3"
+            os.rename(file_path, mp3_file)
+            return mp3_file
+        else:
+            return None
+    except Exception as e:
+        logging.error(f"Error downloading/converting video: {e}")
+        return None
+
+
+async def send_mp3_file(chat_id, video_url):
+    """
+    Function to send an MP3 file to the user with a caption
+    """
+    mp3_file = await download_and_convert_to_mp3(video_url)
+    if mp3_file:
+        # Add a caption to the audio file
+        caption = "Hey your music is here.\n\n➤Bot: @tubyDoo_Bot \n│\n╰┈➤Join @udpcustom"
+        with open(mp3_file, 'rb') as f:
+            await bot.send_audio(chat_id, f, caption=caption)
+        os.remove(mp3_file)  # Remove the MP3 file after sending
 
 
 async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
+    """
+    Main function to initialize and start the bot
+    """
+    # Initialize Bot instance
+    bot = Bot(TOKEN)
+    # Start long-polling
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
