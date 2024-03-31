2747459681011487f80d06faa0c6d854f0689504
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-31 11:55:21 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,6 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram import executor
-
-# Replace with your Flutterwave API keys
-# Replace with your Flutterwave API keys
-# FLUTTERWAVE_PUBLIC_KEY = 'FLWPUBK-0e4658e40b88a018d1451da348f9acab-X'
-# FLUTTERWAVE_SECRET_KEY = 'FLWSECK-2cfcb60ea041cb576453e651c9ee2e43-18e7acefd71vt-X'
-
-# Telegram bot token
-TELEGRAM_TOKEN = '6533833584:AAHPalg1HywEshspXgeGAYOjWRG95jx8X4Q'
-
+from aiogram.middleware.logging import LoggingMiddleware
+from aiogram.utils import executor
 
 # Replace 'YOUR_TOKEN' with your actual bot token
 API_TOKEN = '6533833584:AAHPalg1HywEshspXgeGAYOjWRG95jx8X4Q'
