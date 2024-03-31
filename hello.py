690c31c413db778f09068f041eb4f39539891e9d
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-31 14:29:18 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,52 +1,50 @@
 import asyncio
-import asyncio
 import logging
 import sys
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ParseMode
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+from aiogram.dispatcher.filters import CommandStart
 
 # Bot token can be obtained via https://t.me/BotFather
-#TOKEN = '7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o'
+TOKEN = '7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o'
 
-# Initialize the bot and dispatcher
-bot = Bot(token='7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o')
+# Initialize Bot instance
+bot = Bot(token=TOKEN)
+# Initialize Dispatcher instance
 dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
 
-# Your supporting functions can be integrated here
 
-# Example of an aiogram message handler for the /start command
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    start_message = ("♻️ WELCOME TO TESLA SSH BOT👌. \n"
-                     "━━━━━━━━━━━━━━━━━━━━━━━━━ \n"
-                     "\n"
-                     "You can use me to manage users on your server!\n"
-                     "\n"
-                     "To reload the bot, Press /start\n"
-                     "To see the usage guide, Press /help\n"
-                     "To add user, Press the add user button \n"
-                     "To remove user, Send /remove \n"
-                     "To list users, Press /users \n"
-                     "\n"
-                     "🔰 Made with spirit. \n"
-                     "========================= \n"
-                     "By: @TESLASSH \n"
-                     "Mastered by: @hackwell101 \n"
-                     "Join @udpcustom")
-    await message.answer(start_message)
+@dp.message_handler(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Send a welcome message using message.answer method
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
-# Other message handlers and functions can be integrated similarly
 
-# Start polling
-async def main():
+@dp.message_handler()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward received message back to the sender
+    """
     try:
-        await dp.start_polling()
-    finally:
-        await bot.session.close()
+        # Send a copy of the received message
+        await message.copy_to(chat_id=message.chat.id)
+    except TypeError:
+        # Not all types are supported to be copied, handle it
+        await message.answer("Nice try!")
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+
+async def main() -> None:
+    # Start polling for updates
+    await dp.start_polling()
+
+
+if __name__ == "__main__":
+    # Configure logging
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    # Run main function asynchronously
     asyncio.run(main())
