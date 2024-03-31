76872ea14e7563471cee702c84f4c0550ab9de4c
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-03-31 14:25:59 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,56 +1,52 @@
 import asyncio
+import asyncio
 import logging
 import sys
-from os import getenv
 
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import ParseMode
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = '7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o'
+#TOKEN = '7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o'
 
-# All handlers should be attached to the Router (or Dispatcher)
-dp = Dispatcher()
+# Initialize the bot and dispatcher
+bot = Bot(token='7021922965:AAFgpeUCisXYM-s6rDbzhwBtTNZ62jL0x0o')
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
+# Your supporting functions can be integrated here
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+# Example of an aiogram message handler for the /start command
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    start_message = ("♻️ WELCOME TO TESLA SSH BOT👌. \n"
+                     "━━━━━━━━━━━━━━━━━━━━━━━━━ \n"
+                     "\n"
+                     "You can use me to manage users on your server!\n"
+                     "\n"
+                     "To reload the bot, Press /start\n"
+                     "To see the usage guide, Press /help\n"
+                     "To add user, Press the add user button \n"
+                     "To remove user, Send /remove \n"
+                     "To list users, Press /users \n"
+                     "\n"
+                     "🔰 Made with spirit. \n"
+                     "========================= \n"
+                     "By: @TESLASSH \n"
+                     "Mastered by: @hackwell101 \n"
+                     "Join @udpcustom")
+    await message.answer(start_message)
 
+# Other message handlers and functions can be integrated similarly
 
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
+# Start polling
+async def main():
     try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+        await dp.start_polling()
+    finally:
+        await bot.session.close()
 
-
-async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
