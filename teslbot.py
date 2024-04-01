6db41b17b0c84c5cca37cef1e9ab52d378def50f
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-04-01 17:21:16 +0300

    Update teslbot.py

--- teslbot.py
+++ teslbot.py
@@ -302,9 +302,11 @@ def handle(msg):
         if is_youtube_link(query):
             processing = "Processing... \n Hang on tight🤙"
             processing_message = bot.sendMessage(chat_id, processing)
-            send_mp3_file(chat_id, query)
+            
             bot.deleteMessage((chat_id, msg['message_id']))
             bot.deleteMessage((chat_id, processing_message['message_id']))
+            time.sleep(1)
+            send_mp3_file(chat_id, query, reply_markup=keyboard)
 
         if command.lower() == 'start' or command == '/start':
             start_message = ("♻️ WELCOME TO TESLA SSH BOT👌. \n"
