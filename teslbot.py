f3097f5a9ad7277b4b9bf8e72c917e7af4ebee9e
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-04-02 16:34:50 +0300

    Update teslbot.py

--- teslbot.py
+++ teslbot.py
@@ -302,8 +302,7 @@ def handle(msg):
         if is_youtube_link(query):
             processing = "Processing... \n Hang on tight🤙"
             processing_message = bot.sendMessage(chat_id, processing)
-            send_mp3_file(chat_id, query, reply_markup=keyboard)
-            time.sleep(1)
+            send_mp3_file(chat_id, query)
             bot.deleteMessage((chat_id, msg['message_id']))
             bot.deleteMessage((chat_id, processing_message['message_id']))
 
