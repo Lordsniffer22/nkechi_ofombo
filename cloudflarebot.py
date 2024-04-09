d4382c9aff6bae475f599aeec4dc0bf554b409b7
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-04-09 18:43:25 +0300

    Update cloudflarebot.py

--- cloudflarebot.py
+++ cloudflarebot.py
@@ -1,138 +1,79 @@
-import telepot
-import requests
-from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+#!/bin/bash
+apt install python3-pip
+pip install telepot
+print_yellow() {
+    echo -e "\e[1;33m$1\e[0m"
+}
+print_pink() {
+    echo -e "\e[1;95m$1\e[0m"
+}
+progres() {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+echo -ne "  \033[1;33mWAIT \033[1;37m- \033[1;33m["
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "\033[1;31m#"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "\033[1;33m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "  \033[1;33mWAIT \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+tput cnorm
+}
 
-# Cloudflare API configuration
-CLOUDFLARE_API_KEY = 'a4028ce12fc7e7467b950b69ca480df447ba2'
-CLOUDFLARE_EMAIL = 'tariusblake@gmail.com'
-CLOUDFLARE_ZONE_ID = 'c8b5f50e69aff8cd79d1fea03ad40146'
 
-pending_add_command = {}
+teslbot_fetch() {
+   clear
+   sudo mkdir -p /etc/hlm/flare/
+   sudo rm -f /etc/hlm/flare/flare.py
+   wget -O flare.py https://raw.githubusercontent.com/Lordsniffer22/nkechi_ofombo/main/cloudflarebot.py
+   sleep 3
 
-def get_domain(zone_id):
-    headers = {
-        'X-Auth-Email': CLOUDFLARE_EMAIL,
-        'X-Auth-Key': CLOUDFLARE_API_KEY,
-        'Content-Type': 'application/json',
-    }
+    # [make service file]
+    echo '[Unit]
+    Description=Made by Teslassh (( ZERO ONE LLC ))
+    After=network.target
 
-    response = requests.get(f'https://api.cloudflare.com/client/v4/zones/{zone_id}', headers=headers)
+    [Service]
+    User=root
+    Type=simple
+    ExecStart=/usr/bin/python3 /etc/hlm/flare/flare.py 
+    WorkingDirectory=/etc/hlm/flare/
+    Restart=always
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['result']['name']
-    else:
-        return None
+    [Install]
+    WantedBy=multi-user.target' > /etc/systemd/system/flames.service
+}
 
-def add_dns_record(record_name, record_content):
-    headers = {
-        'X-Auth-Email': CLOUDFLARE_EMAIL,
-        'X-Auth-Key': CLOUDFLARE_API_KEY,
-        'Content-Type': 'application/json',
-    }
-
-    data = {
-        'type': 'A',
-        'name': record_name,
-        'content': record_content,
-        'proxied': False
-    }
-
-    response = requests.post(f'https://api.cloudflare.com/client/v4/zones/{CLOUDFLARE_ZONE_ID}/dns_records', headers=headers, json=data)
-
-    if response.status_code == 200:
-        domain = get_domain(CLOUDFLARE_ZONE_ID)
-        if domain:
-            return f"DNS record {record_name}.{domain} has been created."
-        else:
-            return f"Failed to retrieve domain for zone ID: {CLOUDFLARE_ZONE_ID}"
-    else:
-        return f"Failed to add DNS record. Status code: {response.status_code}\n{response.text}"
-
-def list_dns_records():
-    headers = {
-        'X-Auth-Email': CLOUDFLARE_EMAIL,
-        'X-Auth-Key': CLOUDFLARE_API_KEY,
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.get(f'https://api.cloudflare.com/client/v4/zones/{CLOUDFLARE_ZONE_ID}/dns_records?type=A', headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        records = [f"{record['name']}    -    {record['content']}" for record in data['result']]
-        if records:
-            return "A Records:\n" + "\n".join(records)
-        else:
-            return "No A records found."
-    else:
-        return f"Failed to retrieve DNS records. Status code: {response.status_code}\n{response.text}"
-
-def remove_dns_record(record_name):
-    headers = {
-        'X-Auth-Email': CLOUDFLARE_EMAIL,
-        'X-Auth-Key': CLOUDFLARE_API_KEY,
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.get(f'https://api.cloudflare.com/client/v4/zones/{CLOUDFLARE_ZONE_ID}/dns_records?type=A&name={record_name}', headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        if len(data['result']) == 0:
-            return f"No A record found with name {record_name}"
-        else:
-            record_id = data['result'][0]['id']
-            delete_response = requests.delete(f'https://api.cloudflare.com/client/v4/zones/{CLOUDFLARE_ZONE_ID}/dns_records/{record_id}', headers=headers)
-            if delete_response.status_code == 200:
-                return f"A record {record_name} has been removed."
-            else:
-                return f"Failed to remove A record {record_name}. Status code: {delete_response.status_code}\n{delete_response.text}"
-    else:
-        return f"Failed to retrieve A record for removal. Status code: {response.status_code}\n{response.text}"
-
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-        # Define custom keyboard buttons with smaller size in a single row
-    keyboard = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='Add Record', resize_keyboard=True),
-         KeyboardButton(text='Remove Record', resize_keyboard=True),
-         KeyboardButton(text='List Records', resize_keyboard=True)],
-    ], resize_keyboard=True)
-    
-    if content_type == 'text':
-        command = msg['text']
-        
-        if command.lower() == 'add record':
-            bot.sendMessage(chat_id, "Please enter the DNS record name and IP address in the format [name] [IP address] (e.g., john 192.0.2.1):", reply_markup=keyboard)
-            pending_add_command[chat_id] = 'add record'
-
-        elif command.lower() == 'list records':
-            response = list_dns_records()
-            bot.sendMessage(chat_id, response, reply_markup=keyboard)
-
-        elif command.lower() == 'remove record':
-            try:
-                bot.sendMessage(chat_id, "Hey, to remove a record Please send me the command in the example format below:\n\n/remove john.teslassh.xyz", reply_markup=keyboard)
-            except ValueError:
-                return f"FAILED BOSS"
-        elif command.startswith('/remove'):
-            _, record_name = command.split(' ', 1)
-            response = remove_dns_record(record_name)
-            bot.sendMessage(chat_id, response, reply_markup=keyboard)
-
-        elif chat_id in pending_add_command:
-            record_name, record_content = command.split(' ', 1)
-            response = add_dns_record(record_name, record_content)
-            bot.sendMessage(chat_id, response, reply_markup=keyboard)
-            del pending_add_command[chat_id]
-
-# Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your Telegram Bot token
-bot = telepot.Bot('6486401647:AAGaY2kaQyPKkjVttkjUUeENFk5OxpaJXjE')
-bot.message_loop(handle)
-
-print('Bot is listening...')
-
-while True:
-    pass
-
+print_yellow 'INSTALLING THE PACKAGES'
+progres 'teslbot_fetch'
+initialise() {
+    chmod 640 /etc/systemd/system/flames.service
+    systemctl daemon-reload
+    systemctl enable flames
+    systemctl start flames
+    sleep 4
+}
+print_yellow 'ACTIVATING THE BOT'
+progres 'initialise'
+find / -type f -name "flare.sh" 2>/dev/null | while read -r file;
+    do
+      rm -f "$file"
+    done
+echo ""
+print_pink "THE COLUDFLARE BOT HAS BEEN STARTED SUCCESSFULLY"
+sleep 3
+exit
